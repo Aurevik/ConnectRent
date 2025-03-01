4aa3c2e2bdb097b2114e20ab1479b950aa99e0fe
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2025-03-01 12:51:43 +0530

    Delete duplicate Frontend-main directory

--- Rentalog.in--Frontend-main/src/components/dashboard-components/AddNewRental/AddNewRental.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const AddNewRental = () => {
-    return(
-        <h1>Add New Rental</h1>
-    )
-}
-
-export default AddNewRental;
--- Rentalog.in--Frontend-main/src/components/dashboard-components/CreateNewRental/CreateNewRental.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const CreateNewRental = () => {
-    return(
-        <h1>Create New Rental</h1>
-    )
-}
-
-export default CreateNewRental;
